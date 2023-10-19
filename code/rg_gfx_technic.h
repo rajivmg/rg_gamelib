d0f9698aea244fa80800f34037799275045eb40f
Rajiv Gupta <rajivmg0@gmail.com>
2023-10-19 22:05:03 +0530

    wip materials;

--- code/rg_gfx_technic.h
+++ code/rg_gfx_technic.h
@@ -1,4 +1,22 @@
 #ifndef __RG_GFX_TECHNIC_H__
 #define __RG_GFX_TECHNIC_H__
 
+#include "rg_gfx.h"
+
+RG_BEGIN_RG_NAMESPACE
+RG_BEGIN_GFX_NAMESPACE
+
+struct BaseMaterialTemplate
+{
+    GfxGraphicsPSO* pso;
+};
+
+/*--------
+    gfx::LitMaterialTemplate litMaterialTemplate("lit.hlsl", );
+
+ --------*/
+
+RG_END_GFX_NAMESPACE
+RG_END_RG_NAMESPACE
+
 #endif
