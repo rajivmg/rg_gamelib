3ae2081138fdba66bf165d6ff74be0974e5a7547
Rajiv Gupta <rajivmg0@gmail.com>
2023-12-26 13:44:48 +0530

    add utils.cpp

--- code/utils.h
+++ code/utils.h
@@ -3,9 +3,17 @@
 
 #include "core.h"
 
+// HELPER FUNCTIONS
+// -----------------
+
+
+// INLINE FUNCTIONS
+// ----------------
+
 inline uint32_t roundUp(uint32_t value, uint32_t multiple)
 {
     return (value + multiple - 1) & ~(multiple - 1);
 }
+
 
 #endif // __UTILS_H__
