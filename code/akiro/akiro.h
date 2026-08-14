50da6a70bbc5d5b316a21cd1e3754fcbe2bda9d9
Rajiv Gupta <rajivmg0@gmail.com>
2026-08-14 19:05:09 +0200

    akiro: make eastl work

--- code/akiro/akiro.h
+++ code/akiro/akiro.h
@@ -1,6 +1,20 @@
 #ifndef _AKIRO_H_
 #define _AKIRO_H_
 
-//#include <eastl/vector.h>
+#include <EASTL/string.h>
+#include <EASTL/vector.h>
+#include <EASTL/unordered_map.h>
+#include <EASTL/tuple.h>
+
+struct DefStruct
+{
+	struct Variable
+	{
+		eastl::string type;
+		eastl::string name;
+	};
+};
+
+typedef eastl::unordered_map<eastl::string, DefStruct> StructMap;
 
 #endif
